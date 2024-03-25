4688fba8973bcb9a36d6fc097ef4ccb88f9c6794
itheima <hello@itheima.cn>
2024-03-25 16:12:03 +0800

    修改hello

--- demo/src/main/java/com/example/demo/HelloWorldController.java
+++ demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/hello")
     public String helloWorld(){
+        //
         System.out.println("HelloWorld");
         return "OK";
     }
